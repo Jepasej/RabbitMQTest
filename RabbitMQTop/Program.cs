639784c023495dd66002e79e79611322e85b3dcc
Jepasej <146959451+Jepasej@users.noreply.github.com>
2026-03-19 13:10:49 +0100

    Add project files.

---
+++ RabbitMQTop/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQTop
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost", Port = 5672 };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+            await channel.BasicPublishAsync(exchange: "logs", routingKey: string.Empty, body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+            static string GetMessage(string[] args)
+            {
+                return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+            }
+        }
+    }
+}
